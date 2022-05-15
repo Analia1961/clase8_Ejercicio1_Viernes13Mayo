229f2dd4ff98fecad899920cf2c08faeb8480c19
Analía Alvado <104107403+Analia1961@users.noreply.github.com>
2022-05-15 20:46:33 -0300

    reviso_Ejercicio1 Este ejercicio tiene errores

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,22 @@
 /* • Modifique el código del Ejercicio Identificación mes utilizando arreglos
 Longitud = Length= 12 */
 
-rotulo.innerHTML = "Indique el número de mes que le interesa:";
+//rotulo.innerHTML = "Indique el número de mes que le interesa:";
 
-let miArreglo : string[] = new Array(12);
-miArreglo[0]: "Enero";
-miArreglo[1]= "Febrero";
-miArreglo[2]= "Marzo";
-miArreglo[3]= "Abril";
-miArreglo[4]= "Mayo";
-miArreglo[5]= "Junio";
-miArreglo[6]= "Julio";
-miArreglo[7]= "Agosto";
-miArreglo[8]= "Septiembre";
-miArreglo[9]= "Octubre";
-miArreglo[10]= "Noviembre";
-miArreglo[11]= "Diciembre";
-let nroMes : number = dato.value;
+let miArreglo: string[] = new Array(12);
+miArreglo[0] = "Enero";
+miArreglo[1] = "Febrero";
+miArreglo[2] = "Marzo";
+miArreglo[3] = "Abril";
+miArreglo[4] = "Mayo";
+miArreglo[5] = "Junio";
+miArreglo[6] = "Julio";
+miArreglo[7] = "Agosto";
+miArreglo[8] = "Septiembre";
+miArreglo[9] = "Octubre";
+miArreglo[10] = "Noviembre";
+miArreglo[11] = "Diciembre";
+let nroMes: number = dato.value;
 
-let indice = nroMes -1;
+let indice = nroMes - 1;
 console.log("El mes es:", miArreglo[indice]);
