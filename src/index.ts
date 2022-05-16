06e9aded6ccf49951ee15a15a40bb548c235f369
Analía Alvado <104107403+Analia1961@users.noreply.github.com>
2022-05-16 13:15:08 -0300

    corregi_Ejercicio1 el ejercicio ahora funciona bien

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,8 @@
 /* • Modifique el código del Ejercicio Identificación mes utilizando arreglos
 Longitud = Length= 12 */
 
-//rotulo.innerHTML = "Indique el número de mes que le interesa:";
-
 let miArreglo: string[] = new Array(12);
+
 miArreglo[0] = "Enero";
 miArreglo[1] = "Febrero";
 miArreglo[2] = "Marzo";
@@ -19,7 +18,5 @@ miArreglo[8] = "Septiembre";
 miArreglo[9] = "Octubre";
 miArreglo[10] = "Noviembre";
 miArreglo[11] = "Diciembre";
-let nroMes: number = dato.value;
 
-let indice = nroMes - 1;
-console.log("El mes es:", miArreglo[indice]);
+console.log(miArreglo);
